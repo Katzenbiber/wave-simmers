0ba13f207b7e65503e1d85310341790eaf4c28b2
Christopher Wróbel <wrobel1999@gmail.com>
2024-01-17 00:50:58 +0100

    Add window framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use wgpu::{Instance, InstanceDescriptor};
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::EventLoop,
+    window::Window,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new().unwrap();
+    let window = Window::new(&event_loop).unwrap();
+
+    let instance = Instance::new(InstanceDescriptor::default());
+    let surface = unsafe { instance.create_surface(&window) }.unwrap();
+
+    let _ = event_loop.run(move |event, elwt| match event {
+        Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } => {
+            println!("The close button was pressed; stopping");
+            elwt.exit();
+        }
+        Event::WindowEvent {
+            event: WindowEvent::RedrawRequested,
+            ..
+        } => {
+            // render stuff
+            todo!();
+        }
+        _ => (),
+    });
 }
